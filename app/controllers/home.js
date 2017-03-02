844c1a012fa57d2536308296ef41ec9019490d26
Олег <somebody.jpg@gmail.com>
2017-03-02 21:51:58 +0300

    Редирект на главную страницу

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,10 +1,26 @@
 var express = require('express'),
     router = express.Router();
 
-module.exports = function (app) {
+module.exports = (app) => {
     app.use('/', router);
 };
 
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
     res.render('index.html');
+});
+
+router.get('/top', (req, res, next) => {
+    res.redirect('/');
+});
+
+router.get('/my-music', (req, res, next) => {
+    res.redirect('/');
+});
+
+router.get('/user', (req, res, next) => {
+    res.redirect('/');
+});
+
+router.get('/login', (req, res, next) => {
+    res.redirect('/');
 });
